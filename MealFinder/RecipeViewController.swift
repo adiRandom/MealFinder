f52a93eaa5886afd4c0b8401003f51ed443a7b23
adi_random <me.adi.hf@gmail.com>
2023-04-06 17:33:49 +0300

    Add scroll view and headings

--- MealFinder/RecipeViewController.swift
+++ MealFinder/RecipeViewController.swift
@@ -5,6 +5,12 @@ class RecipeViewController: UIViewController {
 	var mealId: String?
 	var recipe: RecipeModel?
 	
+	private let scrollView: UIScrollView = {
+		let scrollView = UIScrollView()
+		scrollView.translatesAutoresizingMaskIntoConstraints = false
+		return scrollView
+	}()
+	
 	private let mealImageView: UIImageView = {
 		let imageView = UIImageView()
 		imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -28,7 +34,7 @@ class RecipeViewController: UIViewController {
 		let label = UILabel()
 		label.translatesAutoresizingMaskIntoConstraints = false
 		label.textAlignment = .left
-		label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+		label.font = UIFont.italicSystemFont(ofSize: 18)
 		label.textColor = .black
 		label.numberOfLines = 0
 		return label
@@ -44,15 +50,43 @@ class RecipeViewController: UIViewController {
 		return label
 	}()
 	
+	private let ingredientsTitleLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.textAlignment = .left
+		label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+		label.textColor = .black
+		label.text = "Ingredients"
+		return label
+	}()
+	
+	private let instructionsTitleLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.textAlignment = .left
+		label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+		label.textColor = .black
+		label.text = "Instructions"
+		return label
+	}()
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		   
 		view.backgroundColor = .white
-		   
+		
+		view.addSubview(scrollView)
 		view.addSubview(mealImageView)
-		view.addSubview(mealNameLabel)
-		view.addSubview(ingredientsLabel)
-		view.addSubview(instructionsLabel)
+		
+		scrollView.addSubview(mealNameLabel)
+		scrollView.addSubview(ingredientsLabel)
+		scrollView.addSubview(instructionsLabel)
+		scrollView.addSubview(ingredientsTitleLabel)
+		scrollView.addSubview(instructionsTitleLabel)
+		
+		
 
 		setupConstraints()
 		fetchRecipe()
@@ -81,21 +115,38 @@ class RecipeViewController: UIViewController {
 			mealImageView.topAnchor.constraint(equalTo: view.topAnchor),
 			mealImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
 			mealImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-			mealImageView.heightAnchor.constraint(equalTo: view.widthAnchor),
+			mealImageView.heightAnchor.constraint(equalToConstant: 200),
+				
+			scrollView.topAnchor.constraint(equalTo: mealImageView.bottomAnchor),
+			scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+			scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
 			
-			mealNameLabel.topAnchor.constraint(equalTo: mealImageView.bottomAnchor, constant: 8),
+			mealNameLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 8),
 			mealNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
 			mealNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
 			
-			ingredientsLabel.topAnchor.constraint(equalTo: mealNameLabel.bottomAnchor, constant: 8),
+			ingredientsTitleLabel.topAnchor.constraint(equalTo: mealNameLabel.bottomAnchor, constant: 16),
+			ingredientsTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+			ingredientsTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+			
+						
+			ingredientsLabel.topAnchor.constraint(equalTo: ingredientsTitleLabel.bottomAnchor, constant: 8),
 			ingredientsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
 			ingredientsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
 			ingredientsLabel.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
 			
-			instructionsLabel.topAnchor.constraint(equalTo: ingredientsLabel.bottomAnchor, constant: 16),
+			
+			instructionsTitleLabel.topAnchor.constraint(equalTo: ingredientsLabel.bottomAnchor, constant: 16),
+			instructionsTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+			instructionsTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+			
+			
+			instructionsLabel.topAnchor.constraint(equalTo: instructionsTitleLabel.bottomAnchor, constant: 16),
 			instructionsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
 			instructionsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-			instructionsLabel.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+			instructionsLabel.bottomAnchor.constraint(lessThanOrEqualTo: scrollView.bottomAnchor, constant: -16)
 		])
 	}
 	
@@ -111,7 +162,10 @@ class RecipeViewController: UIViewController {
 			mealImageView.kf.setImage(with: resource)
 		}
 		
-		let ingredientsText = recipe.ingredients.map { "\($0.name) ..... \($0.measure)" }.joined(separator: "\n")
+		let ingredientsText = recipe.ingredients
+			.filter { !$0.name.isEmpty && !$0.measure.isEmpty }
+			.map { "\($0.name) ..... \($0.measure)" }
+			.joined(separator: "\n")
 		ingredientsLabel.text = ingredientsText
 	}
 }
